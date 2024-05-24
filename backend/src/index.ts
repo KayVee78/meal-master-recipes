67d559f64f0b0727e1f1e014a29de1a83a7b1076
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-24 09:57:18 +0530

    Implemented API for fetch summary

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 import * as RecipeAPI from "./recipe-api";
@@ -14,6 +14,12 @@ app.get("/api/recipes/search", async (req, res) => {
   const searchTerm = req.query.searchTerm as string;
   const page = parseInt(req.query.page as string);
   const results = await RecipeAPI.searchRecipes(searchTerm, page);
+  return res.json(results);
+});
+
+app.get("/api/recipes/:recipeId/summary", async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const results = await RecipeAPI.getRecipeSummary(recipeId);
   return res.json(results);
 });
 
--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -24,3 +24,24 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
     return null;
   }
 };
+
+export const getRecipeSummary = async (recipeId: string) => {
+  if (!apiKey) {
+    throw new Error("API Key not found");
+  }
+  const url = new URL(
+    `https://api.spoonacular.com/recipes/${recipeId}/summary`
+  );
+  const queryParams = {
+    apiKey,
+  };
+  url.search = new URLSearchParams(queryParams).toString();
+  try {
+    const response = await fetch(url);
+    const resultsJson = await response.json();
+    return resultsJson;
+  } catch (error) {
+    console.error("Error searching for recipes", error);
+    return null;
+  }
+};
