7291cb2d0982e7813836bbd82b9af2b22f386db7
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-27 19:46:46 +0530

    Add and Remove favourites functionality done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import "./App.css";
 import * as api from "./api";
 import { Recipe } from "./types";
 import { RecipeCard } from "./components/RecipeCard";
 import { RecipeModal } from "./components/RecipeModal";
+
+type Tabs = "search" | "favourites";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -11,7 +13,22 @@ const App = () => {
   const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(
     undefined
   );
+  const [selectedTab, setSelectedTab] = useState<Tabs>("search");
+  const [favouriteRecipes, setFavouriteRecipes] = useState<Recipe[]>([]);
   const pageNumber = useRef(1);
+
+  useEffect(() => {
+    const fetchFavouriteRecipes = async () => {
+      try {
+        const favouriteRecipes = await api.getFavouriteRecipes();
+        setFavouriteRecipes(favouriteRecipes.results);
+        console.log("favouriteRecipes", favouriteRecipes);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchFavouriteRecipes();
+  }, []);
 
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -38,26 +55,78 @@ const App = () => {
     }
   };
 
+  const addFavouriteRecipe = async (recipe: Recipe) => {
+    try {
+      await api.addFavouriteRecipe(recipe);
+      setFavouriteRecipes([...favouriteRecipes, recipe]);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const removeFavouriteRecipe = async (recipe: Recipe) => {
+    try{
+      await api.removeFavouriteRecipe(recipe);
+      setFavouriteRecipes(favouriteRecipes.filter((favRecipe) => favRecipe.id !== recipe.id));
+    }catch(e){
+      console.error(e);
+    }
+  }
+
   return (
     <div>
-      <form onSubmit={(e) => handleSearchSubmit(e)}>
-        <input
-          type="text"
-          required
-          placeholder="Enter a search term..."
-          onChange={(e) => setSearchTerm(e.target.value)}
-        ></input>
-        <button>Search</button>
-      </form>
-      {recipes.map((recipe) => (
-        <RecipeCard recipe={recipe} onClick={() => setSelectedRecipe(recipe)} />
-      ))}
-      <button className="view-more-btn" onClick={handleViewMoreClick}>
-        View More
-      </button>
+      <div className="tabs">
+        <h1 onClick={() => setSelectedTab("search")}>Recipe Search</h1>
+        <h1 onClick={() => setSelectedTab("favourites")}>Favourites</h1>
+      </div>
+      {selectedTab === "search" && (
+        <>
+          <form onSubmit={(e) => handleSearchSubmit(e)}>
+            <input
+              type="text"
+              required
+              placeholder="Enter a search term..."
+              onChange={(e) => setSearchTerm(e.target.value)}
+            ></input>
+            <button>Search</button>
+          </form>
+          {recipes.map((recipe) => {
+            const isFavourite = favouriteRecipes.some(
+              (favRecipe) => recipe.id == favRecipe.id
+            );
+            return (
+              <RecipeCard
+                recipe={recipe}
+                onClick={() => setSelectedRecipe(recipe)}
+                onFavouriteButtonClick={isFavourite ? removeFavouriteRecipe : addFavouriteRecipe}
+                isFavourite={isFavourite}
+              />
+            );
+          })}
+          <button className="view-more-btn" onClick={handleViewMoreClick}>
+            View More
+          </button>
+        </>
+      )}
+
+      {selectedTab === "favourites" && (
+        <div>
+          {favouriteRecipes.map((recipe) => (
+            <RecipeCard
+              recipe={recipe}
+              onClick={() => setSelectedRecipe(recipe)}
+              onFavouriteButtonClick={removeFavouriteRecipe}
+              isFavourite={true}
+            />
+          ))}
+        </div>
+      )}
 
       {selectedRecipe ? (
-        <RecipeModal recipeId={selectedRecipe.id.toString()} onClose={()=> setSelectedRecipe(undefined)}/>
+        <RecipeModal
+          recipeId={selectedRecipe.id.toString()}
+          onClose={() => setSelectedRecipe(undefined)}
+        />
       ) : null}
     </div>
   );
--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -1,16 +1,36 @@
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { Recipe } from "../types";
 
 //as we use typescript we can define the props types as below
 interface Props {
   recipe: Recipe;
+  isFavourite: boolean;
   onClick: () => void;
+  onFavouriteButtonClick: (recipe: Recipe) => void;
 }
 
-export const RecipeCard = ({ recipe, onClick }: Props) => {
+export const RecipeCard = ({
+  recipe,
+  onClick,
+  onFavouriteButtonClick,
+  isFavourite,
+}: Props) => {
   return (
     <div className="recipe-card" onClick={onClick}>
       <img src={recipe?.image} alt={recipe?.title} />
       <div className="recipe-card-title">
+        <span
+          onClick={(event) => {
+            event.stopPropagation(); //since we have nested onClick events, if we click on this event it will also trigger the parent onClick event
+            onFavouriteButtonClick(recipe);
+          }}
+        >
+          {isFavourite ? (
+            <AiFillHeart size={25} color="red"/>
+          ) : (
+            <AiOutlineHeart size={25} />
+          )}
+        </span>
         <h3>{recipe.title}</h3>
       </div>
     </div>
